5a503f158fccec1b27cceed902b95cad798dbc95
fahadpro01 <52351395+fahadpro01@users.noreply.github.com>
2021-06-05 03:13:29 +0530

    Add files via upload

---
+++ Fahad astro.py
@@ -0,0 +1,61 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'astro' in command:
+                command = command.replace('astro', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_astro():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'what is' in command:
+        person = command.replace('what is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'tell me about yourself' in command:
+        talk('Iam Astro an Virtual Assistant Created by Fahad or you can call me Fahad girlfriend')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_astro()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Azure
+# ASTRO
+AStro is an Artificial Intelligence Virtual Assistance Just like Alexa and siri . It's still in development stage because it should do a lot of things.
